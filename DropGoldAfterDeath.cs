035991b2f8b0e921bce7da918b1dd0d2c76642bc
exel80 <theexel80@gmail.com>
2019-05-09 18:49:07 +0300

    Cleaning

--- DropGoldAfterDeath.cs
+++ DropGoldAfterDeath.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace DropGoldAfterDeath
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("github.com/exel80/DropGoldAfterDeath", "DropGoldAfterDeath", "1.0.2")]
+    [BepInPlugin("github.com/exel80/DropGoldAfterDeath", "DropGoldAfterDeath", "1.0.3")]
     public class DropGoldAfterDeath : BaseUnityPlugin
     {
         public void Awake()
@@ -21,27 +21,27 @@ namespace DropGoldAfterDeath
                 if (!networkUser && !isMultiplayer()) return;
                 if (component.master.money > 0)
                 {
+                    // Return if player does have a extra life ~ @iDeathHD Thank you for bug report
+                    if (component.master.inventory.GetItemCount(ItemIndex.ExtraLife) >= 1)
+                        return;
+
                     // Get money & alive count
                     uint money = component.master.money;
-                    uint playerCount = Convert.ToUInt16(PlayerCharacterMasterController.instances.Count);
                     List<CharacterMaster> aliveLists = aliveList(component.master);
 
                     // Return if there is no more alive player
                     if (aliveLists.Count < 1)
                         return;
-                    // Return if player does have a extra life ~ @iDeathHD Thank you for bug report
-                    if (component.master.inventory.GetItemCount(ItemIndex.ExtraLife) >= 1)
-                        return;
-
+                    
                     // Take the money and split it
                     component.master.money = 0;
-                    splitMoney(component.master, money, aliveLists);
+                    splitMoney(aliveLists, money);
 
                     // Broadcast drop message<
                     Chat.SendBroadcastChat(new Chat.SimpleChatMessage
                     {
                         baseToken = string.Format("<color=#e2b00b>{0} gold</color> has been dropped! " +
-                        "Alive player(s) received <color=#e2b00b>{1} gold</color> each!", money, (money/Convert.ToUInt16(aliveLists.Count)))
+                        "Alive player(s) received <color=#e2b00b>{1} gold</color> each!", money, (money / Convert.ToUInt32(aliveLists.Count)))
                     });
                 }
             };
@@ -58,28 +58,21 @@ namespace DropGoldAfterDeath
         /// <summary>
         /// Split dead player money to alive players.
         /// </summary>
-        /// <param name="victim">Dead player</param>
         /// <param name="money">Dead player total money</param>
         /// <param name="alive">Alive people count</param>
-        private static void splitMoney(CharacterMaster victim, uint money, List<CharacterMaster> alive)
+        private static void splitMoney(List<CharacterMaster> alive, uint money)
         {
             foreach (CharacterMaster player in alive)
-            {
-                if (player.isClient
-                    && player.isClient
-                    && !player.Equals(victim))
-                {
-                    player.money += (money/Convert.ToUInt16(alive.Count));
-                }
-            }
+                player.money += (money / Convert.ToUInt32(alive.Count));
         }
 
         /// <summary>
-        /// Return aliveList (BUG: include just death people, remember filter it out)
+        /// Return List<CharacterMaster> including only alive player(s).
         /// </summary>
         private static List<CharacterMaster> aliveList(CharacterMaster victim)
         {
-            List<CharacterMaster> listOfBodies = new List<CharacterMaster>();
+            List<CharacterMaster> players = new List<CharacterMaster>();
+
             foreach (PlayerCharacterMasterController player in PlayerCharacterMasterController.instances)
             {
                 if (player.isClient
@@ -87,10 +80,11 @@ namespace DropGoldAfterDeath
                     && player.master.alive
                     && !player.master.Equals(victim))
                 {
-                    listOfBodies.Add(player.master);
+                    players.Add(player.master);
                 }
             }
-            return listOfBodies;
+
+            return players;
         }
     }
 }
