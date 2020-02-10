6f74e72ba871872307673922f46c6890b30efcda
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2020-02-10 01:01:18 -0500

    Additional cleanup

--- DropGoldAfterDeath/DropGoldAfterDeath.cs
+++ DropGoldAfterDeath/DropGoldAfterDeath.cs
@@ -11,33 +11,30 @@ namespace DropGoldAfterDeath
     {
         public void Awake()
         {
-            // Thank you @Storm312 for showing me how this works
             On.RoR2.GlobalEventManager.OnPlayerCharacterDeath += (orig, self, damageReport, networkUser) =>
             {
                 orig(self, damageReport, networkUser);
-                CharacterBody component = damageReport.victim.GetComponent<CharacterBody>();
-
-                if (!networkUser || !isMultiplayer()) return;
+                CharacterBody component = damageReport.victimBody;
+                
+                if (!networkUser || !IsMultiplayer()) return;
                 if (component.master.inventory.GetItemCount(ItemIndex.ExtraLife) > 0) return;
                 if (component.master.money > 0)
                 {
-                    // Get money & alive count
+                    // Get players alive and gold count
+                    List<CharacterMaster> aliveLists = AliveList(component.master);
+                    uint count = Convert.ToUInt32(aliveLists.Count);
                     uint money = component.master.money;
-                    List<CharacterMaster> aliveLists = aliveList(component.master);
 
-                    // Return if there is no more alive player
-                    if (aliveLists.Count < 1)
-                        return;
-                    
-                    // Take the money and split it
-                    component.master.money = 0;
-                    splitMoney(aliveLists, money);
+                    // Return if there is no more alive players
+                    if (count < 1) return;
 
-                    // Broadcast drop message<
+                    // Split gold
+                    SplitMoney(aliveLists, money, count);
+
+                    // Broadcast drop message
                     Chat.SendBroadcastChat(new Chat.SimpleChatMessage
                     {
-                        baseToken = string.Format("<color=#e2b00b>{0} gold</color> has been dropped! " +
-                        "Alive player(s) received <color=#e2b00b>{1} gold</color> each!", money, (money / Convert.ToUInt32(aliveLists.Count)))
+                        baseToken = $"<color=#00FF80>{networkUser.userName}</color> gave everyone <color=#e2b00b>{(money / count)} gold</color> from the grave, how nice!"
                     });
                 }
             };
@@ -46,32 +43,33 @@ namespace DropGoldAfterDeath
         /// <summary>
         /// Return true if more then 1 player in-game
         /// </summary>
-        private static bool isMultiplayer()
+        private static bool IsMultiplayer()
         {
             return PlayerCharacterMasterController.instances.Count > 1;
         }
 
         /// <summary>
-        /// Split dead player money to alive players.
+        /// Split dead player gold to alive players
         /// </summary>
-        /// <param name="money">Dead player total money</param>
-        /// <param name="alive">Alive people count</param>
-        private static void splitMoney(List<CharacterMaster> alive, uint money)
+        /// <param name="alive">List representing players alive</param>
+        /// <param name="money">uint representing gold total of victim</param>
+        /// <param name="count">uint representing players alive</param>
+        private static void SplitMoney(List<CharacterMaster> alive, uint money, uint count)
         {
             foreach (CharacterMaster player in alive)
-                player.money += (money / Convert.ToUInt32(alive.Count));
+                player.money += (money / count);
         }
 
         /// <summary>
-        /// Return List<CharacterMaster> including only alive player(s).
+        /// Return list of alive player(s)
         /// </summary>
-        private static List<CharacterMaster> aliveList(CharacterMaster victim)
+        private static List<CharacterMaster> AliveList(CharacterMaster victim)
         {
             List<CharacterMaster> players = new List<CharacterMaster>();
 
             foreach (PlayerCharacterMasterController player in PlayerCharacterMasterController.instances)
             {
-                if (player.isPlayerControlled
+                if (player.isConnected
                     && player.master.alive
                     && !player.master.Equals(victim))
                 {
