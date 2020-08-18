18ff1de03215a3b98f7152ef9b0b55903319896a
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2020-08-18 19:08:09 -0400

    Update for RoR2 1.0 update!

--- DropGoldOnDeath/DropGoldOnDeath.cs
+++ DropGoldOnDeath/DropGoldOnDeath.cs
@@ -1,12 +1,14 @@
 ﻿using BepInEx;
+using R2API.Utils;
 using RoR2;
 using System;
 using System.Collections.Generic;
 
 namespace DropGoldOnDeath
 {
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.DifferentModVersionsAreOk)]
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("dev.tsunami.DropGoldOnDeath", "DropGoldOnDeath", "1.0.6")]
+    [BepInPlugin("dev.tsunami.DropGoldOnDeath", "DropGoldOnDeath", "1.1.0")]
     public class DropGoldOnDeath : BaseUnityPlugin
     {
         public void Awake()
@@ -20,6 +22,12 @@ namespace DropGoldOnDeath
                 if (component.master.inventory.GetItemCount(ItemIndex.ExtraLife) > 0) return;
                 if (component.master.money > 0)
                 {
+                    // Pick a random quip to add a little humor
+                    string[] quips =
+                        {"Don't forget to thank them!", "Everybody point and laugh!", "How kind of them!", "Maybe next time...", "Someone didn't heal enough!", "What were they thinking?!", "We're rich!"};
+                    Random rand = new Random();
+                    int index = rand.Next(quips.Length);
+
                     // Get players alive and gold count
                     List<CharacterMaster> aliveLists = AliveList(component.master);
                     uint count = Convert.ToUInt32(aliveLists.Count);
@@ -35,7 +43,7 @@ namespace DropGoldOnDeath
                     // Broadcast drop message
                     Chat.SendBroadcastChat(new Chat.SimpleChatMessage
                     {
-                        baseToken = $"<color=#00FF80>{networkUser.userName}</color> gave everyone <color=#e2b00b>{(money / count)} gold</color> from the grave, how nice!"
+                        baseToken = $"<color=#00FF80>{networkUser.userName}</color> gave everyone <color=#e2b00b>{(money / count)} gold</color> from the grave! {quips[index]}"
                     });
                 }
             };
--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # DropGoldOnDeath
-A Risk of Rain 2 mod that splits your gold among remaining alive players.
+A Risk of Rain 2 mod that splits your gold among remaining alive players. All players are required to have this mod to achieve the intended effect.
 
 ## Features
 This mod that splits your gold among remaining alive players. This is an improved fork of the original [DropGoldAfterDeath](https://thunderstore.io/package/exel80/DropGoldAfterDeath/) mod, by [exel80](https://github.com/exel80), with the following changes:
-- Updated to work with the latest R2API as of publishing
+- Updated to work with the latest R2API
+- Code restructured to reduce unnecessary checks
 - Trimmed output down to only 1 line of chat
-- Fixed null/multiplayer checks
-- Reduced calculations
 
 ## Installation
 1. Install [BepInEx](https://thunderstore.io/package/bbepis/BepInExPack/) and [R2API](https://thunderstore.io/package/tristanmcpherson/R2API/).
@@ -14,6 +13,10 @@ This mod that splits your gold among remaining alive players. This is an improve
 3. Launch the game and enjoy! To remove you simply need to delete the `DropGoldOnDeath.dll` file.
 
 ## Changelog
+### Version 1.1.0
+- Updated for Risk of Rain 2 version 1.0
+- Adds in funny quips to each death message
+
 ### Version 1.0.6
 - Artifacts update!
 
